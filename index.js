d9b8aaa450652bee4c9b5cccaba34a159c860722
VarozXYZ <alvaropiro@gmail.com>
2025-10-04 18:49:54 +0200

    working swap data fetch

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ import 'dotenv/config'
 import { createJupiterApiClient } from '@jup-ag/api';
 
 // Creamos constantes básicas a partir del archivo .env
-const sol_pk = process.env.SOLANA_PRIVATE_KEY;
+const sol_private_key = process.env.SOLANA_PRIVATE_KEY;
+const sol_public_key = process.env.SOLANA_PUBLIC_KEY;
 const rpc = process.env.SOLANA_RPC;
 
 // Instanciamos el cliente de Jupiter
 const jupiter = createJupiterApiClient();
 
 // Creamos parámetros del trade que queremos hacer
-const params = {
+const quote_params = {
     inputMint: "So11111111111111111111111111111111111111112", // SOL
     outputMint: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v", // USDC
     amount: 10000000, // 0.1 SOL
@@ -19,10 +20,42 @@ const params = {
 };
 
 // Intentamos conseguir el quote
-const quote = await jupiter.quoteGet(params)
+const quote = await jupiter.quoteGet(quote_params)
 
+// Si no hay quote correcto, lanzamos error y terminamos el programa
 if (!quote) {
-    console.log("Failed to get quote!")
-} else {
-    console.log("Quote recieved:", quote)
+    throw new Error("Failed to get quote!")  
 }
+// Sacamos los datos del quote por consola
+console.log("Quote recieved:", quote)
+
+// Definimos los datos del Swap para hacer la llamada usando el quote recibido
+
+const swap_params = {
+    swapRequest: {
+      quoteResponse: quote,
+      userPublicKey: sol_public_key,
+      dynamicComputeUnitLimit: true,
+      dynamicSlippage: true,
+      prioritizationFeeLamports: {
+        priorityLevelWithMaxLamports: {
+          maxLamports: 350000,
+          priorityLevel: "veryHigh",
+        },
+      },
+    },
+  }
+
+// Hacemos otro fetch a la API para conseguir los datos de la transaccion
+const swap_data = await jupiter.swapPost(swap_params)
+
+// Si no hay swap data correcto, lanzamos error y terminamos el programa
+if (!swap_data) {
+    throw new Error("Failed to get swap params!")  
+}
+// Sacamos los datos del quote por consola
+console.log("Swap data recieved:", swap_data)
+
+
+
+
